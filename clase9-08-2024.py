4a1edebe0f8f1363dafcf964b6628edcad78bf1e
ledesma716 <juandavid.716@gmail.com>
2024-08-10 10:01:40 -0500

    clase_9_de_agosto

---
+++ clase9-08-2024.py
@@ -0,0 +1,66 @@
+# num1 = int(input("ingrese el primer numero:"))
+# num2 = int(input("ingrese el primer numero:"))
+# es_valida =(num1 % 2 == 0) and (num1> 0) and (num1>num2)
+# print(f"numero {num1} es par {es_valida}, positivo y es mayor {num2}? {es_valida}")
+
+# #1
+
+# num1 = int(input("ingrese el primer numero:"))
+# num2 = int(input("ingrese el segundo numero:"))
+# es_mayor =  num1 >num2
+# print(f"numero {num1} es mayor {num2}? {es_mayor}")
+
+# #2
+
+# num = int(input("ingrese un numero:"))
+# es_par = num % 2 == 0
+# print(f"el numero {num} es par? {es_par}")
+
+# #3
+
+# num = float(input("ingrese un numero:"))
+# el_rango = 10 <= num <= 20
+# print(f"el numero {num} esta entre 10 y 20 ? {el_rango}")
+
+# #4
+
+# num1 = float(input("ingrese el primer numero:"))
+# num2 = float(input("ingrese el segundo numero:"))
+# son_iguales = num1 == num2
+# print(f"el primer numero {num1} es igual al segundo numero {num2}? { son_iguales}")
+
+# #5
+
+# num = float(input("ingrese un numero:"))
+# no_mayor = not num > 50
+# print(f"¿el numeron {num} NO es mayor que 50 ? {no_mayor}")
+
+# #1
+
+# num = int(input("ingrese un numero: "))
+# es_positivo_y_par = num > 0 and num % 2 == 0
+# print(f"el numero {num} es posotivo y par? {es_positivo_y_par}")
+
+# #2
+
+# num = int(input("ingrese un numero: "))
+# es_negativo_y_par = num < 0 or num % 2 != 0
+# print(f"el numero {num} es negativo y impar? {es_negativo_y_par}")
+
+# #3
+
+# num = int(input("ingrese un numero: "))
+# no_en_rango = not(10<=num<=20)
+# print(f"el numero {num} No esta entre 10 y 20 ? {no_en_rango}")
+
+# #4
+
+# num = int(input("ingrese un numero: "))
+# multiplo_de_tres_no_de_cinco = num % 3 == 0 and num % 5 != 0
+# print(f"el numero {num} es multiplo de 3 y no de 5? {multiplo_de_tres_no_de_cinco} ")
+
+# #5
+
+# num = int(input("ingrese un numero: "))
+# no_multiplo_de_cuatro_o_es_positivo = num % 4 != 0 or num > 0 
+# print(f"el numero {num} no es multiplo de 4 o es positivo? {no_multiplo_de_cuatro_o_es_positivo}")
