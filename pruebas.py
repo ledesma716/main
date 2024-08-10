c717f4403d10c1e19d0f508a5fe42e3292f5bd06
ledesma716 <juandavid.716@gmail.com>
2024-08-10 10:08:04 -0500

    pruebas

---
+++ pruebas.py
@@ -0,0 +1,72 @@
+#es cierto que 5 es mayor que 3
+mayor= 5>3
+print(mayor)
+
+#c es menor que 1000
+c=12
+menor = c<1000
+print(menor)
+
+#a y d son iguales
+a= 4
+d= 4
+if d==a:
+    print("son iguales")
+
+#d es igual a 6
+d=6
+if d==6 :
+    print("es igual")
+
+#c es mayor o igual a 12
+c = 13
+if c >= 12:
+    print("es mayor o igual a 12")
+
+#c mas d son menor o igual a 10
+c=5
+d=5
+total= c+d
+if total <= 10:
+    print("es menor o igual a 10")
+
+#f y d son menores que 5
+f=4
+d=4
+if f <5 and d < 5 :
+    print("ambos son menores")
+
+#f es mayor que 1000 o d es 250
+f= 1050
+d= 250
+if f>1000 or d == 250:
+    print("es verdadero")
+else:
+    print("es falso")
+
+#es cierto que 10 es menor o igual a 10
+diez=10<=10
+print(diez)
+
+#es cierto que a es mayor que b
+a=10
+b=5
+print(a>b)
+
+
+#si nombre es iguala a ana copia responde buenos dias
+nombre=input("introduce tu nombre:")
+if nombre == "ana" :
+    print("¡buenos dias!")
+
+#pregunta cual es una comida favorita si responde pastel pon coge un pastel si no pon coge una galleta
+torta=input("cual es tu comida favorita:")
+
+if torta == "pastel":
+    print("coge un pastel")
+else:
+    print("coge una galleta")
+
+
+
+
